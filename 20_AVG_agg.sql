cb60fb74fe7c3817e683cc810b1512c2f2ff3ca8
Rahul <159618613+rahulvenkat207@users.noreply.github.com>
2025-02-20 18:22:05 +0530

    Add files via upload

---
+++ 20_AVG_agg.sql
@@ -0,0 +1,5 @@
+-- returns the average of values in a column, works only with numeric columns,Nulls are ignored
+-- find the average score of all customer
+
+SELECT AVG(score) AS Average_Score
+FROM customers
