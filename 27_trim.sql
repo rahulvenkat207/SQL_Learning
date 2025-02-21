098f507660a1ac4a73e46966e3bf0511444d70a4
Rahul <159618613+rahulvenkat207@users.noreply.github.com>
2025-02-21 15:26:23 +0530

    Add files via upload

---
+++ 27_trim.sql
@@ -0,0 +1,6 @@
+-- list all the last name of all customers and remove all the white spaces in the names
+-- Trim() - remove leading and trailing white spaces from a string
+-- LTrim() - Left space trim
+-- RTrim() -eight space trim
+SELECT trim(last_name)
+FROM customers
